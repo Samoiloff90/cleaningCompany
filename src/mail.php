e976a84976008c74f9cf843bf9883fbf2d2407b9
Eugene Samoilov <e.e.samoilov@mail.ru>
2020-09-15 18:30:02 +0400

    correction

--- src/mail.php
+++ src/mail.php
@@ -13,12 +13,12 @@ $form_subject = trim($_POST["form_subject"]);
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'bigov88@list.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'ITaA3uta1^nN'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'Username@list.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'pasword'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('bigov88@list.ru'); // от кого будет уходить письмо?
+$mail->setFrom('Username@list.ru'); // от кого будет уходить письмо?
 $mail->addAddress('e.e.samoilov@mail.ru');     // Кому будет уходить письмо 
 $mail->isHTML(true);                                  // Set email format to HTML
 
